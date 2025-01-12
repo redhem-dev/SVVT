ea1beb983e5465470060b2882c4d5b2e780913b7
Edhem Rogo <edhem.rogo@stu.ibu.edu.ba>
2025-01-12 20:12:54 +0100

    HTTP Enforcement Test

---
+++ src/test/java/HTTP_EnforcementTest/HTTP_EnforcementTest.java
@@ -0,0 +1,55 @@
+package HTTP_EnforcementTest;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HTTP_EnforcementTest {
+
+    private static WebDriver webDriver;
+
+    private static String baseUrl;
+
+    private static JavascriptExecutor js;
+
+    @BeforeAll
+    public static void setUp() {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/edhemrogo/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        webDriver = new ChromeDriver(options);
+        baseUrl = "https://www.ue.ba";
+        js = (JavascriptExecutor) webDriver;
+        webDriver.get(baseUrl);
+        webDriver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void testHttpsEnforcing() throws InterruptedException {
+        // Step 1: Navigate to the HTTP version of the URL
+        String httpUrl = baseUrl.replace("https://", "http://");
+        webDriver.get(httpUrl);
+        webDriver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        // Step 2: Verify the redirection to HTTPS
+        String currentUrl = webDriver.getCurrentUrl();
+        assertTrue(currentUrl.startsWith("https://"), "The site should redirect to HTTPS");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        // Close the browser
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+}
